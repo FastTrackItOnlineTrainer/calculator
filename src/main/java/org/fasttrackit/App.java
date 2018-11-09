bd8386cc0d5a6e16e5a3d75145a5fba8d87983bb
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2018-11-09 15:44:33 +0200

    overloaded calculator methods

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -15,7 +15,22 @@ public class App {
         System.out.println("The product is " + product);
 
         System.out.println("Dividing " + x + " by " + y);
-        double result = calculator.divide(x, y);
+        int result = calculator.divide(x, y);
         System.out.println("The result is " + result);
+
+        double m = 127.55;
+        double n = 323.99;
+
+        System.out.println("Calculating the sum of " + m + " and " + n);
+        double doubleSum = calculator.sum(m, n);
+        System.out.println("The sum is " + doubleSum);
+
+        System.out.println("Calculating the product of " + m + " and " + n);
+        double doubleProduct = calculator.multiply(m, n);
+        System.out.println("The product is " + doubleProduct);
+
+        System.out.println("Dividing " + m + " by " + n);
+        double doubleResult = calculator.divide(m, n);
+        System.out.println("The result is " + doubleResult);
     }
 }
